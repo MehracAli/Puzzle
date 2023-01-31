7531255aaa507afc37872458decb42095f141b5d
Mehrac Mammadov <mehrajvm@code.edu.az>
2023-01-31 10:41:47 +0400

    added win function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let pieces = document.querySelectorAll(".puzzle-piece")
 let boxes = document.querySelectorAll(".drop-zone")
-
+let count = 0
 pieces.forEach((piece) =>{
     piece.addEventListener("dragstart", function (e) {
         e.dataTransfer.setData("id", e.target.id)
@@ -15,12 +15,22 @@ boxes.forEach((box) =>{
 
     box.addEventListener("drop", function (e) {
         let id = e.dataTransfer.getData("id")
+        let dataId = box.getAttribute("data-id")
+        
         let draggable = document.getElementById(id)
-        box.append(draggable)
+        element = box.appendChild(draggable)
+        
+        if(id == dataId){
+            count++
+            if(count == 9){
+                let win = document.getElementById("win")
+                win.style.display = "block"
+            }
+        }
     })
     box.addEventListener("drop", function (e) {
         let id = e.dataTransfer.getData("id")
         let draggable = document.getElementById(id)
-        box.append(draggable)
+        element = box.appendChild(draggable)
     })
 })
